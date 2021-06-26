18382e5af8ccd50c0d640b2190cbfe0ab50d006b
Isadora <isadorasouza@protonmail.ch>
2021-06-25 23:25:59 -0300

    first update javascript document

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,30 @@
-console.log('oi')
+// div -> class linha *** ok
+// div -> coluna especificar classe
+// span
+// div linha -> país + img+span
+
+function criarTemplateLinha(){
+    // CRIANDO LINHA DO QUADRO
+    let linha = document.createElement("div")
+
+    // ADICIONANDO CLASSE PARA A DIV LINHA
+    linha.classList.add("linha")
+    
+    // COLUNAS
+    
+    // PRIMEIRA COLUNA DO RANK
+    let colunaRank = document.createElement("div")
+    colunaRank.classList.add("coluna", "coluna-rank")
+
+    // SPAN TÍTULO RANK
+    let colunaRankTitulo = document.createElement("span")
+    colunaRankTitulo.innerText = "1º"
+    
+    colunaRank.appendChild(colunaRankTitulo)
+    console.log(colunaRank)
+    console.log(colunaRankTitulo)
+}
+
+criarTemplateLinha()
+
+// continuar estudos a partir de 1h:26min
